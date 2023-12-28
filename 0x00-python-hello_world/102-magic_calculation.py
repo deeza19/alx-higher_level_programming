44cddf7013ae2a4e8a5cf3491787a7a2e4ffadec
deeza19 <zainabadisa425@gmail.com>
2023-12-28 06:32:03 -0800

    13

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 import dis
-
 def magic_calculation(a, b):
     return len(a, b)
+
+import marshal
+fd = open("102-magic_calculation.py", "rb")
+magic = fd.read(4) # python version specific magic num
+date = fd.read(4) # compilation date
+code_object = marshal.load(fd)
+fd.close()
