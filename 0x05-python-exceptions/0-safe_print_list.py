98a48c41af6b971c52074bac3b7e0fff77bb4ecc
deeza19 <zainabadisa425@gmail.com>
2024-04-03 07:59:38 -0700

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    
+    try:
+        my_list=[1,4,5]
+        x = my_list[:5]
+        print(x)
+
+    except IndexError:
+        print("Numbers are not up to 4")
