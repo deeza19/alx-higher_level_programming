9636fb72c7fcff1976f2678d4ebca34eee5ecbc5
deeza19 <zainabadisa425@gmail.com>
2024-04-03 08:56:24 -0700

    int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        value = 100
+        print("{:d}".format(value))
+            return True:
+                if value is int
+
+                else return False
+    except:
+        print("Value is not an integer")
