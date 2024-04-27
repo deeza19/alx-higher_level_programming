b6435c84208b66c6466a831dd0d6da05dbf40add
deeza19 <zainabadisa425@gmail.com>
2024-04-27 14:55:53 -0700

    islower

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         print("{:0d}{:0d}".format(i, j), end=", ")
-print(end="\n")
+print("89", end="\n")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    for alpha in range(a, z):
+        if c == alpha:
+            return True
+        return False
