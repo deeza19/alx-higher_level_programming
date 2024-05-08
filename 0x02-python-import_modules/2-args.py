d02d2ac2e25f5b340e72ce579872fb93663f3d8c
deeza19 <zainabadisa425@gmail.com>
2024-05-08 04:06:06 -0700

    import mod

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
 def print_arguments():
+
+
     i = 1
     num_arguments = len(sys.argv) - 1
     if num_arguments < 1:
@@ -11,6 +13,8 @@ def print_arguments():
             print("{:d}: {:s}".format(i, sys.argv[i]), sep="\n")
         elif num_arguments > 1:
             print("{} arguments:".format(num_arguments))
+
+
             print("{:d}: {:s}".format(i, sys.argv[i], sep="\n"))
 if __name__ == "__main__":
     print_arguments()
