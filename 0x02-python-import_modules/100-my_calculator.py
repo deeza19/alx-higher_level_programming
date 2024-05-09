e70807d361b407a5fee85e2b1f5ca26d84490cb6
deeza19 <zainabadisa425@gmail.com>
2024-05-09 14:16:48 -0700

    calculate

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def calculator():
+    operator = add, sub, mul, div
+    if Args != 3:
+        print("Usage: ./100-my_calculator.py {<>] {<>} {<>} = {<>}".format(a, b, operator, result)
+    exit 1
+    elif operator == NULL:
+         print("Unknown operator. Available operators: +, -, * and /")
+    exit 1
+
+
+if __name__ == "__main__":
+    Arguments = int(a, b)
