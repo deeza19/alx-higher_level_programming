a92cd2ca3116c68b9cdd6216cc304db72c684002
deeza19 <zainabadisa425@gmail.com>
2024-05-09 13:21:53 -0700

    pycode

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
+
+
 def add_arguments(*args):
     result = 0
     for arg in args:
         result += arg
     return result
+
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
