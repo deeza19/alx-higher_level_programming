3ea564b1ab936b8a8e2d92e7291a5d77e23c8e0d
deeza19 <zainabadisa425@gmail.com>
2024-05-12 15:50:23 -0700

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     
     try:
         my_list=[1, 4, 5]
-        x = input(my_list[:4])
-        print(my_list[:x])
-
-    except IndexError:
-        print("Numbers are not up to 4")
+        for x in my_list[:x]:
+            print(x, end="")
+        print(end="\n")
+            
+    except IndexError.my_list:
+        print("Numbers are not up to {:d}".format(x))
