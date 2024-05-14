bce04f6143f742f1ce64bd41e87958b589303ce1
deeza19 <zainabadisa425@gmail.com>
2024-05-14 15:16:45 -0700

    more classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is a module."""
+
+
+class Rectangle:
+    """This is a class called Rectangle."""
+    def __init__(self, __width):
+        self.__width = __width
+
+    def width(self):
+        pass
+
+    def width(self, value):
+        self.value = value
+        try:
+            width == int(width)
+        except TypeError:
+            raise
+            print("height must be an integer")
+
+        if width < 0:
+            except ValueError:
+                raise
+                print("width must be >= 0"
