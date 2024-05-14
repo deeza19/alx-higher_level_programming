c13ca821355f99a9598c92739d5aff13ce02cd7b
deeza19 <zainabadisa425@gmail.com>
2024-05-14 16:10:34 -0700

    modules

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,14 +7,6 @@ class Square:
     """Created a class square with private instance attribute size."""
     def __init__(self, __size):
         self.__size = __size
-
-class SquareValidation(Exception):
-    """A user-defined exception class."""
-    def __init__(self, size=0):
-        Square.__init__(self, __size)
-        Exception.__init__(self)
-        self.size = size
-    try:
         self.size = int(size)
         if size != int(size):
             raise SquareValidation(TypeError)
