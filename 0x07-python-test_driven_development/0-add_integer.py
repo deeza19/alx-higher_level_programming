5862635e9632e11c882e19ae46cdb0c3f98b77eb
deeza19 <zainabadisa425@gmail.com>
2024-05-17 00:14:54 +0000

    test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a module."""
+
+
+def add_integer(a, b=98):
+    if type(a, b) not in [int, float]:
+        raise TypeError("a must be an integer") or ("b must be an integer")
+    
+    elif (a, b) in [float]:
+        return int(a, b)
+    return a + b
