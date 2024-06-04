e7985e75ed54807edc677d2a84ea1939a802742b
deeza19 <zainabadisa425@gmail.com>
2024-06-04 05:39:08 -0700

    first element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-
-    idx = len(my_list)
     if idx < 0:
-        return (None)
-    elif idx > len(my_list):
-        return (None)
+        return None
+    elif idx > range(my_list):
+        return None
