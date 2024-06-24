d15b3e194cd433705a1ef462971ec4dc064e2c4f
deeza19 <zainabadisa425@gmail.com>
2024-06-24 00:23:34 -0700

    changed else to elif

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         if ord(char) >= 97:
             result += chr(ord(char) - 32)
             print("{}".format(result))
-        else ord(char) >= 65 and ord(char) <= 90:
+        elif ord(char) >= 65 and ord(char) <= 90:
             print("{}".format(result))
