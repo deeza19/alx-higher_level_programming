14669d6937c6fa6d3f650dcbd44501dfa2b9e3ec
deeza19 <zainabadisa425@gmail.com>
2024-08-14 03:25:38 -0700

    print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 
-    try:
-        print("{:d}".format(my_list[:x], sep=""))
-        print("//n", )
-    except TypeError:
-        print("Numbers are not up to {:d}".format(x))
+    try: 
+        print(my_list[0:x-1], sep="")
+    except IndexError:
+        print("Number of elements is not up to {:d}".format(x))
