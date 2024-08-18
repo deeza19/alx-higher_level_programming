0b5e68d1f096fae8bb5660035e5ce69ea78c84b7
deeza19 <zainabadisa425@gmail.com>
2024-08-18 03:04:55 -0700

    indexerror

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    i = 0
     for i in my_list:
-        if my_list != None:
-            print("{:d}".format(int(my_list[-1])))
-            i -= 1
-        else:
+        if i <= (len(my_list) - 1) and my_list != None: 
+            i += 1
+            reversed_num = my_list[-1-i]
+            print("{:d}".format(int(reversed_num)))
+        elif IndexError and True:
             return None
