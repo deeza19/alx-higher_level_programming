93197ec4a8d32411e705d52d5d956a24bb34536f
deeza19 <zainabadisa425@gmail.com>
2024-08-18 04:46:59 -0700

    No c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    ascii_c = ord('c')
+    ascii_C = ord('C')
+    new_chars = []
+    for char in my_string:
+        if ord(char) not in (ascii_c, ascii_C):
+            new_chars.append(char)
+            new_string = ''.join(new_chars)
+            return new_string
