8ce4a1c7543135df97e8ed2dbc0f84c08ebb4add
deeza19 <zainabadisa425@gmail.com>
2024-08-18 04:01:59 -0700

    replace element in copied list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     new_list[idx] = element
-    for idx in my_list:
+    for idx in new_list:
         if idx < 0:
             return new_list
-        elif idx > len(my_list):
+        elif idx > len(new_list):
             return new_list
         else:
             return new_list
