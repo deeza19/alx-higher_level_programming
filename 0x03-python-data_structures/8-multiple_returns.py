1e6a3b5d45f03148d265fc0ae3f66fb421528d52
deeza19 <zainabadisa425@gmail.com>
2024-08-23 10:28:19 -0700

    multiple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == None:
+        sentence[0] = None
+    else:
+        sentence = len(sentence), sentence[0]
+        return sentence
