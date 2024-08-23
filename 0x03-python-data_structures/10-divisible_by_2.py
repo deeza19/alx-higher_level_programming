4c7b576894e1274d1e24055c23483d1b04ed6262
deeza19 <zainabadisa425@gmail.com>
2024-08-23 11:26:21 -0700

    max int

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     for num in my_list:
-        new_list = [num % 2 == 0]
-        if num //2:
-            return new_list
+       new_list = [num % 2 == 0]
+       return new_list
