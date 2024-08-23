47f4de8628013e4e5b8fb8f97f1e8227a3a8757c
deeza19 <zainabadisa425@gmail.com>
2024-08-23 10:38:30 -0700

    ret

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        sentence[:1] = None
+        return None
     else:
         sentence = len(sentence), sentence[0]
         return sentence
