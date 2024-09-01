1b917f304b4ef671ec47120863eee53bb7de5665
deeza19 <zainabadisa425@gmail.com>
2024-09-01 09:13:35 -0700

    score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    high_score = a_dictionary[1]
     for key in a_dictionary[key]:
+        high_score = a_dictionary[key]
         if a_dictionary[key] > high_score:
             high_score = key
         elif a_dictionary is None:
