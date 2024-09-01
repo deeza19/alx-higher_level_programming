f6c3c8f592a3683f80449a7ca397159916326537
deeza19 <zainabadisa425@gmail.com>
2024-09-01 09:07:52 -0700

    score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    high_score = dictionary[0]
+    for key in a_dictionary[key]:
+        if a_dictionary[key] > high_score:
+            high_score = key
+        elif a_dictionary is None:
+            return None
+        else:
+            return None
