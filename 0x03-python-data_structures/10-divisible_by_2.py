4a9c1a75938934f435cba0a3f10ece6e6ec4322f
deeza19 <zainabadisa425@gmail.com>
2024-09-02 09:57:22 -0700

    div

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     mult_2 = []
-    for num in my_list:
+    for num in range(0, len(my_list)):
         if num % 2 == 0:
             mult_2.append(num)
         else:
