f9184ed0260d674cbbd6db6618f4aee87f419d26
deeza19 <zainabadisa425@gmail.com>
2024-09-02 09:02:12 -0700

    char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
     for char in my_string:
         if ord(char) not in (ascii_c, ascii_C):
             new_chars.append(char)
-        new_string = ''.join(new_chars)
+    new_string = ''.join(new_chars)
     return new_string
