43afbcdf8e5a22fe2247145926eeb2c3f6815eba
deeza19 <zainabadisa425@gmail.com>
2024-09-02 09:36:22 -0700

    none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,8 @@
 def multiple_returns(sentence):
     if sentence is None:
         return None
+    elif sentence == "":
+        return None, None
     else:
         sentence = len(sentence), sentence[0]
         return sentence
