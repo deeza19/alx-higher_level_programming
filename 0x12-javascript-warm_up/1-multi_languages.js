041a8975e4eae2ecdfeb3c4955fee89beb7ee244
deeza19 <zainabadisa425@gmail.com>
2024-09-10 15:35:47 -0700

    multi line js file print

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let multiSentence = 'C is fun';
+let part2 = 'Python is cool';
+let part3 = 'JavaScript is amazing';
+console.log(multiSentence);
+console.log(part2);
+console.log(part3);
