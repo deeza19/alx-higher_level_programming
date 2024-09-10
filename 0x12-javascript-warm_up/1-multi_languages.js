8ee10e54413bf054d1eb5e1b91a84f0a987485d2
deeza19 <zainabadisa425@gmail.com>
2024-09-10 15:38:03 -0700

    semi standard fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let multiSentence = 'C is fun';
-let part2 = 'Python is cool';
-let part3 = 'JavaScript is amazing';
+const multiSentence = 'C is fun';
+const part2 = 'Python is cool';
+const part3 = 'JavaScript is amazing';
 console.log(multiSentence);
 console.log(part2);
 console.log(part3);
