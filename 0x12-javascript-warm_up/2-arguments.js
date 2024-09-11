7ab9d1d59318381ccead33a6d4534e5dadbc7ebb
deeza19 <zainabadisa425@gmail.com>
2024-09-11 08:21:08 -0700

    semi standard fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function printMessage (...args) {
-  if (args.length === None) {
+  if (args.length === 0) {
     console.log('No argument');
   } else if (args.length === 1) {
     console.log('Argument found');
