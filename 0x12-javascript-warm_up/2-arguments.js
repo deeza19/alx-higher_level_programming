94c4340100bc17918994e0092f2d52b56d17ce78
deeza19 <zainabadisa425@gmail.com>
2024-09-11 08:03:31 -0700

    semi standard fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-function printMessage() {
-  if (arguments.length === 0) {
+function printMessage (...args) {
+  if (args.length === 0) {
     console.log('No argument');
-  } else if (arguments.length === 1) {
+  } else if (args.length === 1) {
     console.log('Argument found');
   } else {
     console.log('Arguments found');
   }
 }
+
+printMessage();
